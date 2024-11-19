27772e854fa1fac33f1559b9da24f8a8af1fa3d7
Ross Hvidsten <rhvidste@c1r3p3.hive.fi>
2024-11-19 09:48:12 +0200

    fixed header file reference and norminette errors in includes

--- include/ft_print.h
+++ include/ft_print.h
@@ -5,24 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rhvidste <rhvidste@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/15 14:07:58 by rhvidste          #+#    #+#             */
-/*   Updated: 2024/11/19 09:15:57 by rhvidste         ###   ########.fr       */
+/*   Created: 2024/11/19 09:33:11 by rhvidste          #+#    #+#             */
+/*   Updated: 2024/11/19 09:46:55 by rhvidste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#ifndef FT_PRINT_H
+# define FT_PRINT_H
 
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdio.h>
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdio.h>
 
-int		ft_printchar(int c);
-int		ft_printstr(const char *str);
-int		ft_printptr(void *ptr);
-int		ft_printnbr(int nb);
-int		ft_printunsigned(unsigned int n);
-int		ft_print_hex(unsigned int num, const char format);
-int		ft_printpercent(void);
+int	ft_printchar(int c);
+int	ft_printstr(const char *str);
+int	ft_printptr(void *ptr);
+int	ft_printnbr(int nb);
+int	ft_printunsigned(unsigned int n);
+int	ft_print_hex(unsigned int num, const char format);
+int	ft_printpercent(void);
 
 #endif
